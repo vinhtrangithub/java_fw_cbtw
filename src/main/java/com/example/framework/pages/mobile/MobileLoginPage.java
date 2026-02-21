9f5a6c1e30dc6278dfb7b2eec687459d1c13ea2c
Vinh Tran <tuanvinh1008@gmail.com>
2026-02-21 19:21:40 +0700

    init code

--- src/main/java/com/example/framework/pages/mobile/MobileLoginPage.java
+++ src/main/java/com/example/framework/pages/mobile/MobileLoginPage.java
@@ -1,9 +1,6 @@
 package com.example.framework.pages.mobile;
 
 import com.example.framework.core.BasePage;
-import io.appium.java_client.pagefactory.AndroidFindBy;
-import io.appium.java_client.pagefactory.iOSXCUITFindBy;
-import org.openqa.selenium.WebElement;
 
 public class MobileLoginPage extends BasePage {
     
